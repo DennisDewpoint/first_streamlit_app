70cc2d7dbd7ab963ae1c50c9c428125284fb4d11
DennisDewpoint <123739252+DennisDewpoint@users.noreply.github.com>
2023-08-15 12:55:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-import requests
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -22,6 +22,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+import requests
 
 streamlit.header("Fruityvice Fruit Advice!")
 
