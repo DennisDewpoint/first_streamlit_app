d26fa83444381397089656f0ca4e447b3b6507fe
DennisDewpoint <123739252+DennisDewpoint@users.noreply.github.com>
2023-08-22 11:46:15 +0200

    Update streamlit_app.py
    reorder

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -22,9 +24,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
-import requests
-
 
 streamlit.header('Fruityvice Fruit Advice!')
 
